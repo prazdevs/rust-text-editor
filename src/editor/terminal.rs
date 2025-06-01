84c0db031e56c1e7ed4e0fbbc0f2aac06631b000
Prazdevs <prazdevs@uninbox.me>
2025-06-01 02:34:05 +0200

    feat: handle location/position and keep track of it

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -11,10 +11,10 @@ pub struct Size {
     pub width: usize,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Default)]
 pub struct Position {
-    pub x: usize,
-    pub y: usize,
+    pub col: usize,
+    pub row: usize,
 }
 
 pub struct Terminal {}
@@ -28,7 +28,6 @@ impl Terminal {
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(Position { x: 0, y: 0 })?;
         Self::execute()?;
         Ok(())
     }
@@ -49,20 +48,20 @@ impl Terminal {
         Ok(())
     }
 
-    /// Move cursor to given position.
+    /// Move caret to given position.
     /// `Position` will be truncated to `u16::MAX` if bigger.
-    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
+    pub fn move_caret_to(position: Position) -> Result<(), Error> {
         #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
-        Self::queue_command(MoveTo(position.x as u16, position.y as u16))?;
+        Self::queue_command(MoveTo(position.col as u16, position.row as u16))?;
         Ok(())
     }
 
-    pub fn show_cursor() -> Result<(), Error> {
+    pub fn show_caret() -> Result<(), Error> {
         Self::queue_command(Show)?;
         Ok(())
     }
 
-    pub fn hide_cursor() -> Result<(), Error> {
+    pub fn hide_caret() -> Result<(), Error> {
         Self::queue_command(Hide)?;
         Ok(())
     }
